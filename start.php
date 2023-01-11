ecca2645d240ec27e272f8b8df33636eb7a3077b
Judith Awino Omondi <judith.awino.omondi@gmail.com>
2023-01-11 13:14:09 +0300

    Option 2

--- start.php
+++ start.php
@@ -1,23 +1,45 @@
 <?php
 
+// $min = 1; 
+// $max = 50;
+// $guess = -3; //Change this value to test!
+// $num = rand($min, $max);
+
+// switch (true) {
+//     case ($guess==$num):
+//         echo "You are correct, the number is $num";
+//         break;
+//     case ($guess<$num && $guess>=$min):
+//         echo "Too low, the number is $num";
+//         break;
+//     case ($guess>$num && $guess<=$max):
+//         echo "Too high, the number is $num";
+//         break;
+//     default:
+//         echo "$guess is out of range";
+//         break;
+// }
 $min = 1; 
 $max = 50;
-$guess = -3; //Change this value to test!
+$guess = 34; //Change this value to test!
 $num = rand($min, $max);
-
+// $num = 16; // Change this value to test!
 switch (true) {
     case ($guess==$num):
         echo "You are correct, the number is $num";
         break;
-    case ($guess<$num && $guess>=$min):
+    case ($guess!=$num):
+    if ($guess < $num && $guess>=1){
         echo "Too low, the number is $num";
-        break;
-    case ($guess>$num && $guess<=$max):
+    }else if ($guess > $num && $guess<=50){
         echo "Too high, the number is $num";
-        break;
+    }
     default:
+    if ($guess<$min || $guess>$max){
         echo "$guess is out of range";
-        break;
+    }
+    break;
 }
 
+// If you really want to go nuts, try this:
 
